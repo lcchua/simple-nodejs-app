d222413c4d8268a80f449f6fcb79455e656cbbc9
lcchua <laichwang@gmail.com>
2024-10-13 17:46:29 +0800

    adjust providers versions

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.60.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.6.3"
+      version = "~> 3.6.0"
     }
   }
 
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.9"
 }
 
 # Configure the AWS Provider
